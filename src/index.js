1ff26bc044bb3f04fbcfc7182f1bcc65dda5d0cd
Oksana Martynenko <oks.martynenko@gmail.com>
2021-06-05 18:44:51 +0300

    Добавляет стартовые файлы

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import './sass/main.scss';
+import './index.css';
+
+//https://restcountries.eu/rest/v2/name/{name}
+fetch('https://restcountries.eu/rest/v2/name/Ukraine').then(responce => {
+  return responce.json();
+})
+.then(country => {
+  console.log(country);
+});
