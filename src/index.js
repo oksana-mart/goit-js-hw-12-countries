8a929758246be1c88e01d9a99508b5a4648e04fe
Oksana Martynenko <oks.martynenko@gmail.com>
2021-06-05 21:33:29 +0300

    Добавляет разметку полученных данных

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import './index.css';
+//import API from './js/fetchCountries.js';
+import countryCard from './templates/countryCard.hbs';
+//import countriesList from './templates/countriesList.hbs';
 
-//https://restcountries.eu/rest/v2/name/{name}
-fetch('https://restcountries.eu/rest/v2/name/Ukraine').then(responce => {
+const refs = {
+  cardContainer: document.querySelector('.js-card-container')
+}
+
+fetch('https://restcountries.eu/rest/v2/name/Canada').then(responce => {
   return responce.json();
 })
 .then(country => {
   console.log(country);
+  const markup = countryCard(...country);
+  console.log(markup);
+  refs.cardContainer.innerHTML = markup;
+})
+.catch(error => {
+  console.log(error);
 });
