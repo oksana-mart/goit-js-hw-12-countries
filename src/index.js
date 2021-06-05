a480c6cfac989a2fb9a1f333b7d879bcc3f3effd
Oksana Martynenko <oks.martynenko@gmail.com>
2021-06-06 02:33:23 +0300

    Добавляет разметку для отображения списка 2-10 стран

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './index.css';
 //import API from './js/fetchCountries.js';
 import countryCard from './templates/countryCard.hbs';
-//import countriesList from './templates/countriesList.hbs';
+import countriesList from './templates/countriesList.hbs';
 var debounce = require('lodash.debounce');
 
 const refs = {
@@ -30,7 +30,19 @@ function fetchCountry(countryName) {
 }
 
 function renderCountryCard (country) {
-  const markup = countryCard(...country);
-  refs.cardContainer.innerHTML = markup;
+  console.log(country);
+
+  if (country.length > 10) {
+    
+  }
+  else if (country.length >=2 && country.length <= 10) {
+    const countriesMarkup = countriesList(country);
+    refs.cardContainer.innerHTML = countriesMarkup;
+  }
+  else if (country.length === 1) {
+    const markup = countryCard(...country);
+    console.log(markup);
+    refs.cardContainer.innerHTML = markup;
+  }
 }
 
