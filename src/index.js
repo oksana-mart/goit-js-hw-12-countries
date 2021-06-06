056ea5946cd8d953057150b2d02b1c84095c592f
Oksana Martynenko <oks.martynenko@gmail.com>
2021-06-07 00:50:50 +0300

    Поправляет alert

--- src/index.js
+++ src/index.js
@@ -3,14 +3,9 @@ import API from './js/fetchCountries.js';
 import countryCard from './templates/countryCard.hbs';
 import countriesList from './templates/countriesList.hbs';
 
-import { alert, defaultModules } from '@pnotify/core';
+import { alert } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
-import * as PNotifyBootstrap4 from '@pnotify/bootstrap4';
-import '@pnotify/bootstrap4/dist/PNotifyBootstrap4.css';
-
-defaultModules.set(PNotifyBootstrap4, {});
-
-//const debounce = require('lodash.debounce');
+import '@pnotify/core/dist/BrightTheme.css';
 
 const refs = {
   debounce: require('lodash.debounce'),
@@ -22,21 +17,19 @@ refs.inputSearch.addEventListener('input', refs.debounce(onInputSearch, 500));
 
 function onInputSearch(e) {
   e.preventDefault();
-
   const searchQuery = e.target.value;
-  console.log(searchQuery);
+
   if (searchQuery.length === 0) {
-    return refs.cardContainer.innerHTML = '';
-    
+    refs.cardContainer.innerHTML = '';
+    return;
   }
   API.fetchCountry(searchQuery)
     .then(renderCountryCard)
-    .catch(onFetchError);
-    //.finally(() => );
+    .catch(onFetchError)
+    .finally(() => refs.inputSearch.value = '');
 }
 
 function renderCountryCard(country) {
-  //console.log(country);
   if (country.length > 10) {
     alert('Too many matches found. Please enter a more specific query!');
   }
@@ -46,7 +39,6 @@ function renderCountryCard(country) {
   }
   else if (country.length === 1) {
     const markup = countryCard(...country);
-    //console.log(markup);
     refs.cardContainer.innerHTML = markup;
   }
 }
