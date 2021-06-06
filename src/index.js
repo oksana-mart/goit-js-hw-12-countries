4af6f5c26bb737f9e26937c21f8b62e95cd89531
Oksana Martynenko <oks.martynenko@gmail.com>
2021-06-07 00:52:25 +0300

    Добавляет readme

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './index.css';
 import API from './js/fetchCountries.js';
 import countryCard from './templates/countryCard.hbs';
 import countriesList from './templates/countriesList.hbs';
-
 import { alert } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
