d08eccb1ba9316ca925b741a80d65504d3997599
Oksana Martynenko <oks.martynenko@gmail.com>
2021-06-07 12:35:16 +0300

    Корректирует условие функции

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -2,10 +2,9 @@ const BASE_URL = 'https://restcountries.eu/rest/v2';
 
 function fetchCountries(countryName) {
   return fetch(`${BASE_URL}/name/${countryName}`).then(responce => {
-    if (!responce.ok) {
-      return error;
-    }
-    return responce.json(); 
+    if (responce.ok) {
+      return responce.json(); 
+    }   
   },
   );
 }
